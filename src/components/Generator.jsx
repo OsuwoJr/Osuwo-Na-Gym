cdfeb0e707a1319aecff39fd2107b5843b00e2ad
OsuwoJr <johnfredosuwo@gmail.com>
2024-07-05 12:14:06 +0300

    Created SectionWrapper.jsx component

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import SectionWrapper from './SectionWrapper'
 
 const Generator = () => {
   return (
     <div className='min-h-screen'>
-     
+     <SectionWrapper>
+        
+     </SectionWrapper>
     </div>
   )
 }
