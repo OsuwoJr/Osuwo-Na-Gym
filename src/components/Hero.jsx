fdfca53b2be61ad78a94b6cd18d6603217f1b7d0
OsuwoJr <johnfredosuwo@gmail.com>
2024-07-05 11:12:30 +0300

    Updated Hero.jsx
    P,h1,P

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-      Hero
+      <p>IT'S TIME TO BE</p>
+      <h1>A Bazuu <span>Unfat</span>!</h1>
+      <p>I hereby aceept with  whole my heart,chest,stomach,liver,brain,soul,lungs and any other preciously valued organ in my body.<span>To stay fit and be lean like never before.</span>Not to partake any stupidly processed diet and <span>to strictly follow</span> my gym lessons every day! </p>
     </div>
   )
 }
