83d64bc2d4fd8fff256d9f1fddb7acebd20350f6
OsuwoJr <johnfredosuwo@gmail.com>
2024-07-05 11:17:29 +0300

    updated Hero.jsx
    Centered the div

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center'>
       <p>IT'S TIME TO BE</p>
       <h1>A Bazuu <span>Unfat</span>!</h1>
       <p>I hereby aceept with  whole my heart,chest,stomach,liver,brain,soul,lungs and any other preciously valued organ in my body.<span>To stay fit and be lean like never before.</span>Not to partake any stupidly processed diet and <span>to strictly follow</span> my gym lessons every day! </p>
