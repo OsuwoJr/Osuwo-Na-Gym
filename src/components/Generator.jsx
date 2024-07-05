823f915e837e5dcdbcc1a33e30241738884349ca
OsuwoJr <johnfredosuwo@gmail.com>
2024-07-05 13:19:18 +0300

    Updated Generator.jsx
    Header with index,title and description

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import SectionWrapper from './SectionWrapper'
+function Header (props){
+    const {index, title, description} = props
+    return (
+        <div>
+            <div>
+              <p>{index}</p>
+              <p>{title}</p>
+            </div>
+            <p>{description}</p>
+        </div>
+    )
+}
 
 const Generator = () => {
   return (
     <div className='min-h-screen'>
      <SectionWrapper header={"generate your workout"} title={['It\'s', 'Huge', 'o\'clock']}>
-       Generator
+       <Header index={'01'} title={'Pick your poison'} description={"Select the workout you wish to endure"}/>
      </SectionWrapper>
     </div>
   )
