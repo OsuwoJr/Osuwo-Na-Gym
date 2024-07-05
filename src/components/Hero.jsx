34c35df1522c803ef63b7dc5fa66233d24630345
OsuwoJr <johnfredosuwo@gmail.com>
2024-07-05 11:21:42 +0300

    Updated Hero.jsx
    h1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className='min-h-screen flex flex-col gap-10 items-center justify-center'>
       <p>IT'S TIME TO BE</p>
-      <h1>A Bazuu <span>Unfat</span>!</h1>
+      <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>A Bazuu <span>Unfat</span>!</h1>
       <p>I hereby aceept with  whole my heart,chest,stomach,liver,brain,soul,lungs and any other preciously valued organ in my body.<span>To stay fit and be lean like never before.</span>Not to partake any stupidly processed diet and <span>to strictly follow</span> my gym lessons every day! </p>
     </div>
   )
