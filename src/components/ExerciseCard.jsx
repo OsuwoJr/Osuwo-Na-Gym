517e23a5fdea02850c2f26fb737f0fbbb47e9010
OsuwoJr <johnfredosuwo@gmail.com>
2024-07-10 14:38:27 +0300

    Updated ExerciseCard.jsx

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const ExerciseCard = (props) => {
     const {exercise, i} = props
+    const[setsCompleted,setSetsComplete]= useState(0)
+    function handleSetIncrement(){
+        setSetsComplete((setsCompleted + 1) % 6)
+    }
+    
   return (
     <div className='p-4 rounded-md flex flex-col gap-4 bg-slate-950 sm:flex-wrap'>
       <div className='flex flex-col sm:flex-row sm:items-center sm:flex-wrap gap-x-4'>
@@ -19,13 +24,18 @@ const ExerciseCard = (props) => {
         {['reps', 'rest','tempo'].map(info => {
             return (
                 <div key={info} className='flex flex-col p-2 rounded border-[1.5px] border-solid border-slate-900 w-full '>
-                    <h3>{info === 'reps' ? `${exercise.unit}`: info}</h3>
+                    <h3 className='capitalize text-slate-400 text-sm'>{info === 'reps' ? `${exercise.unit}`: info}</h3>
 
                     <p className='font-medium'>{exercise[info]}</p>
                 </div>
             )
         })}
+        <button onClick={handleSetIncrement} className='flex flex-col p-2 rounded border-[1.5px] duration-200 border-solid border-blue-900 hover:border-blue-600 w-full '>
+        <h3 className='capitalize text-slate-400 text-sm'>Sets Completed</h3>
+        <p className='font-medium'>{setsCompleted} / 5</p>
+        </button>
       </div>
+
 
     </div>
   )
