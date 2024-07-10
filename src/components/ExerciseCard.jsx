7c47d6b48057d5f7a6865fe3634297110715b4d7
OsuwoJr <johnfredosuwo@gmail.com>
2024-07-10 13:52:45 +0300

    Update ExerciseCard.jsx

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -18,7 +18,9 @@ const ExerciseCard = (props) => {
       <div className='grid grid-cols-2 sm:grid-cols-4 sm:place-items-center gap-2'>
         {['reps', 'rest','tempo'].map(info => {
             return (
-                <div key={info} className='flex flex-col p-2 rounded border-[1.5px] border-solid border-slate-900 w-full '></div>
+                <div key={info} className='flex flex-col p-2 rounded border-[1.5px] border-solid border-slate-900 w-full '>
+                    <h3></h3>
+                </div>
             )
         })}
       </div>
